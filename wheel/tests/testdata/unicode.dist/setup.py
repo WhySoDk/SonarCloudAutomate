c8e4754bbc82642f260a1584dd3dcc0c2bf23b9b
WhySoDk <thanat.phichitphanphong@gmail.com>
2024-02-19 13:25:39 +0700

    Delete pypa/wheel

--- wheel/tests/testdata/unicode.dist/setup.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-from setuptools import setup
-
-setup(name='unicode.dist',
-      version='0.1',
-      description=u'A testing distribution \N{SNOWMAN}',
-      packages=['unicodedist']
-      )
