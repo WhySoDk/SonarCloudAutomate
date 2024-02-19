877fd1817b887f4f1d8656cde819852d3ef50ddd
WhySoDk <thanat.phichitphanphong@gmail.com>
2024-02-19 13:33:49 +0700

    Added pypa/wheel

---
+++ wheel/tests/testdata/commasinfilenames.dist/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="testrepo",
+    version="0.1",
+    packages=["mypackage"],
+    description="A test package with commas in file names",
+    include_package_data=True,
+    package_data={"mypackage.data": ["*"]},
+)
