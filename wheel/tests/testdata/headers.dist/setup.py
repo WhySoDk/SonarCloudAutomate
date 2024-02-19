730af4e7ff0afbb49a04a11b8a875212be3195a8
WhySoDk <thanat.phichitphanphong@gmail.com>
2024-02-19 13:33:41 +0700

    Delete pypa/wheel

--- wheel/tests/testdata/headers.dist/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="headers.dist",
-    version="0.1",
-    description="A distribution with headers",
-    headers=["header.h"],
-)
--- wheel/tests/testdata/unicode.dist/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="unicode.dist",
-    version="0.1",
-    description="A testing distribution \N{SNOWMAN}",
-    packages=["unicodedist"],
-)
