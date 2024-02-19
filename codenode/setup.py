1d31760711f8c511fdfe1102d7459ce3322eafbc
WhySoDk <thanat.phichitphanphong@gmail.com>
2024-02-19 16:58:44 +0700

    Added 0xf0f/codenode

---
+++ codenode/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='codenode',
+    version='1.0',
+    packages=find_packages(),
+    url='https://github.com/0xf0f/codenode',
+    license='MIT',
+    author='0xf0f',
+    author_email='0x0meta@gmail.com',
+    description='a simple framework for code generation'
+)
