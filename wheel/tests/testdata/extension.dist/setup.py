217d5bf24aa516408290b88ee23bca52b6d7f002
WhySoDk <thanat.phichitphanphong@gmail.com>
2024-02-19 13:33:06 +0700

    Added pypa/wheel

---
+++ wheel/tests/testdata/extension.dist/setup.py
@@ -0,0 +1,8 @@
+from setuptools import Extension, setup
+
+setup(
+    name="extension.dist",
+    version="0.1",
+    description="A testing distribution \N{SNOWMAN}",
+    ext_modules=[Extension(name="extension", sources=["extension.c"])],
+)
